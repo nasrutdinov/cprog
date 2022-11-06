8587c2e2db1f759146f257da28c2f7e908b45bd7
marat <mnasrutd@gmail.com>
2022-11-06 22:11:28 +0300

    add file list.cpp

---
+++ list.cpp
@@ -0,0 +1,82 @@
+﻿// list.cpp : ручное создание списка (двустороннего)
+//
+
+#include "pch.h"
+#include <iostream>
+
+struct list {
+	int x;
+	list *next;
+	list *pred;
+};
+
+int main()
+{
+	list *p, *start, *finish;
+	//list.x = 1;
+	finish = new list;
+	finish->x = 1;
+	finish->next = NULL;
+	finish->pred = NULL;
+	start = finish;
+
+	for (int i = 2; i < 10; i++) {
+		finish->next = new list;
+		finish->next->x = i;
+
+		//Ссылка на предыдущий элемент
+		finish->next->pred = finish;
+
+		//Заменяем ссылку на конечный элемент
+		finish = finish->next;
+		finish->next = NULL;
+	}
+
+	p = start;
+	while (p) {
+		std::cout << p->x << "\n";
+		p = p->next;
+	}
+
+	//удаление
+	p = start;
+	while (p->x!=5) {
+		p = p->next;
+	}
+
+	//Удаление элемента
+	p->pred->next = p->next;
+	p->next->pred = p->pred;
+	delete p;
+
+	std::cout << "new list \n";
+	p = start;
+	while (p) {
+		std::cout << p->x << "\n";
+		p = p->next;
+	}
+
+	//вставка элемента в список?
+
+	p = start;
+	while (p->x != 4) {
+		p = p->next;
+	}
+
+	list *q = new list;
+	q->x = 55;
+
+	q->next = p->next;
+	q->pred = p;
+	p->next = q;
+
+	std::cout << "new list 2 \n";
+	p = start;
+	while (p) {
+		std::cout << p->x << "\n";
+		p = p->next;
+	}
+
+
+ //   std::cout << "Hello World!\n"; 
+}
