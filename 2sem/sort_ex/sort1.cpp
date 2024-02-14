3098f34e0a4619c86de61d8ab65d331082d30d86
marat <47354871+nasrutdinov@users.noreply.github.com>
2024-02-14 09:58:26 +0300

    Update sort1.cpp

--- 2sem/sort_ex/sort1.cpp
+++ 2sem/sort_ex/sort1.cpp
@@ -5,27 +5,28 @@
 #include <fstream>
 #include <vector>
 
-void array_print(std::vector<int> a, int size);
+void array_print(std::vector<int> a);
 
 int main()
 {
-    std::ifstream f("file.txt");
-    
-    int size=0;
-    f >> size;
-    
-    std::vector<int> a(size);
+    std::ifstream input_file("file.txt");
 
-    if (f.is_open()) {
-        std::cout << "ok! \n";
+    if (!input_file.is_open()) {
+        std::cout << "file not open! \n";
+        return 1;
     }
 
+        
+    int size=0;
+    input_file >> size;
+    std::vector<int> a(size);
+
     for (int k = 0; k < size; ++k) {
-        f >> a[k];
+        input_file >> a[k];
     }
 
     std::cout << "array before sorting: " << std::endl;
-    array_print(a, size);
+    array_print(a);
 
 
     for (int k = 0; k < size; ++k) {
@@ -37,13 +38,13 @@ int main()
     }
 
     std::cout << "array after sorting: " << std::endl;
-    array_print(a, size);
+    array_print(a);
 
  }
 
-void array_print(std::vector<int> a, int size) {
+void array_print(std::vector<int> a) {
     //std::cout << "array after sorting: " << std::endl;
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < a.size(); ++i) {
         std::cout << a[i] << "\t";
     }
     std::cout << std::endl;
